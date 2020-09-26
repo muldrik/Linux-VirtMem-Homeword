985aa52b3fa086440269c0cade870888f1ef990d
muldrik <muldrik@yandex.ru>
2020-09-26 18:46:15 +0300

    Add Fifo test template

---
+++ src/test/kotlin/FifoTests.kt
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.*
+import java.io.File
+import java.lang.IllegalArgumentException
+import kotlin.test.assertEquals
+import kotlin.test.assertFailsWith
+
+/**
+ * Executes unit tests; Has to be called manually
+ */
+class FifoTests {
+    @Test
+    fun `simple test`(){
+    }
+
+}
